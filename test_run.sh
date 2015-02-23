c210e8c15afd535b71627ad412d40a9744c93e47
Stephen Newhouse <stephen.j.newhouse@gmail.com>
2015-02-23 15:59:57 +0000

    minor

--- test_run.sh
+++ test_run.sh
@@ -6,15 +6,11 @@ docker \
 run \
 --rm=true \
 -v ${HOME}/pgrs:/home/pipeman \
--w /home/pipeman \
--e HOME=/home/pipeman \
--e USER=pipeman \
---user pipeman \
 --name prsice \
 -i \
--t compbio/prsice:1.2 R -q --file=/usr/local/bin/PRSice_v1.2.R --args \
+-t compbio/prsice:1.2 R -q --file=/usr/local/bin/PRSice_v1.2.multicore.R --args \
 plink /usr/local/bin/plink1.9_x86_64 \
 base /usr/local/bin/TOY_BASE_GWAS.assoc \
 target /usr/local/bin/TOY_TARGET_DATA \
-slower 0 supper 0.5 sinc 0.01 covary F \
+slower 0 supper 0.5 sinc 0.01 covary F ncpu 30 \
 figname /home/pipeman/EXAMPLE_1
